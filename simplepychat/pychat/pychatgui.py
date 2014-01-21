d04d84330c8c903e4b50ceb9c95b34eb27aae813
keyvin <keyvin@gmail.com>
2014-01-21 09:37:58 -0500

    Added the connect dialog and made it model.

--- simplepychat/pychat/pychatgui.py
+++ simplepychat/pychat/pychatgui.py
@@ -22,18 +22,57 @@ class pychatgui():
         self.tkentry = Entry(self.rightframe)
         self.tktext = Text(self.rightframe)
         self.tklistbox = Listbox(self.leftframe)        
-        self.menubar = Menu(self.tkframe)
-        self.menubar.add_command(label="Connect", command=self.tkframe.quit )
         self.tklistbox.pack(fill=Y, expand=YES)
         self.tktext.pack(fill=BOTH, expand=YES)
         self.tkentry.pack(fill=X, expand=NO)
         self.tkentry.bind('<Return>', self.entryhandler)
+        self.makemenu()
         self.tkframe.config(menu=self.menubar)
+        self.tktext.config(state=DISABLED)
         self.tkframe.mainloop()
         
     def entryhandler(self, event):
+        self.tktext.config(state=NORMAL)
         self.tktext.insert(END, self.tkentry.get()+'\n')
         self.tktext.yview(END)
+        self.tktext.config(state=DISABLED)
         self.tkentry.delete(0, END)
+        
+    def makemenu(self):
+        
+        self.menubar = Menu(self.tkframe)
+        filemenu = Menu(self.menubar, tearoff=0)
+        filemenu.add_command(label="Connect", command=self.connectmenu)
+        filemenu.add_command(label="Quit", command=self.tkframe.quit)
+        self.menubar.add_cascade(label="File", menu=filemenu)
+
+    def connectmenu(self):
+        connection = connectdialog(self.tkframe)
+        self.tkframe.wait_window(connection.top)
+        if connection.connected == True:
+            pass
+
+        
+class connectdialog():
+    def __init__(self, parent):
+        self.connected = False
+        self.top = Toplevel(parent)
+        
+        Label(self.top, text="Server").pack()
+        self.sentry = Entry(self.top)
+        self.sentry.pack()
+        Label(self.top, text="Port").pack()
+        self.pentry = Entry(self.top)
+        self.pentry.pack()
+        Button(self.top, text="Connect", command=self.connectbutton).pack()
+        Button(self.top, text="Cancel", command=self.top.destroy).pack()
+    def connectbutton(self):
+        self.connected = True
+        self.server = self.sentry.get()
+        self.port = self.pentry.get()
+        self.top.destroy()
 
 n = pychatgui()
+
+
+    
